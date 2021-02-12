93a589a19a17285f0e293567f5b7b0a1ac8899d0
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-02-12 10:37:39 +0300

    🧨 Type fix

--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
-import { OptionDefinitions, Result } from "bargs";
+export type OptionDefinitions = {
+    name: string;
+    type: (t: unknown) => unknown;
+    aliases?: string[];
+    default?: boolean;
+}[];
+
+export interface Result {
+    _unknown: {
+        _?: string;
+        [key: string]: unknown;
+    };
+    [key: string]: unknown;
+}
 
 export function bargs(
     options: OptionDefinitions,
